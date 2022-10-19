eac9250549866c2963601efaa3dcf1adf7206533
EwaMazurek <ewa.mmazurek@gmail.com>
2022-10-19 18:07:18 +0200

    last chance

--- src/js/products.js
+++ src/js/products.js
@@ -1,16 +1,18 @@
-function myFunction(d, p, b) {
-  var x = document.getElementById(d);
-  var y = document.getElementById(p);
-  var m = document.getElementById(b);
+{
+  function myFunction(d, p, b) {
+    var x = document.getElementById(d);
+    var y = document.getElementById(p);
+    var m = document.getElementById(b);
 
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-    y.style.display = 'none';
-    m.style.backgroundImage = 'none';
-  } else {
-    x.style.display = 'none';
-    y.style.display = 'block';
-    m.style.backgroundImage =
-      'url("https://raw.githubusercontent.com/Adamskoczen/Project-1-IceCream/main/src/images/test1.png")';
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+      y.style.display = 'none';
+      m.style.backgroundImage = 'none';
+    } else {
+      x.style.display = 'none';
+      y.style.display = 'block';
+      m.style.backgroundImage =
+        'url("https://raw.githubusercontent.com/Adamskoczen/Project-1-IceCream/main/src/images/test1.png")';
+    }
   }
 }
